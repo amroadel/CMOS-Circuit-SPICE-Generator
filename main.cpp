bbbb689696e6bdd2e0454a094955e652e35771e0
amroadel <amroadel@aucegypt.edu>
2020-05-13 23:41:34 +0200

    ADDED: new_label() new_string()

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 
 enum Mtype { PMOS , NMOS };
-int m_number = 1;
+int m_number = 0;
+int curr_label = 0;
 struct CMOS {
     string m_name;
     string drain;
@@ -15,9 +16,18 @@ struct CMOS {
     Mtype type; 
 };
 
+struct wire {
+    string node1;
+    string node2;
+};
+
 void NOT (string, string, queue<CMOS>);
+void NOR (string, string, string, queue<CMOS>);
+void NAND (string, string, string, queue<CMOS>);
 void OR (string, string, string, queue<CMOS>);
 void AND (string, string, string, queue<CMOS>);
+string new_label();
+string new_mosfet();
 
 int main(int argc, char *argv[])
 {
@@ -29,15 +39,14 @@ void NOT (string input, string output, queue<CMOS> deck)
 {
     CMOS m1, m2; 
 
-    m1.m_name = "M" + to_string(m_number);
+    m1.m_name = new_mosfet();
     m1.drain = output;
     m1.gate = input;
     m1.source = "VDD"; 
     m1.body = "VDD";
     m1.type = PMOS;
-    m_number++;
     
-    m2.m_name = "M" + to_string(m_number);
+    m2.m_name = new_mosfet();
     m2.drain = output;
     m2.gate = input;
     m2.source = "0"; 
@@ -48,6 +57,16 @@ void NOT (string input, string output, queue<CMOS> deck)
     deck.push(m2);
 }
 
+void NOR (string op1, string op2, string output,  queue<CMOS> deck)
+{
+
+}
+
+void NAND (string op1, string op2, string output, queue<CMOS> deck)
+{
+
+}
+
 void OR (string op1, string op2, string output,  queue<CMOS> deck)
 {
 
@@ -56,4 +75,16 @@ void OR (string op1, string op2, string output,  queue<CMOS> deck)
 void AND (string op1, string op2, string output, queue<CMOS> deck)
 {
 
+}
+
+string new_label()
+{
+    curr_label++;
+    return "L" + to_string(curr_label);
+}
+
+string new_mosfet()
+{
+    m_number++;
+    return "M" + to_string(m_number);
 }
